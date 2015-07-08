ce5e18051e99944b4afb0fdc11091f0f180eb4e0
futuremarc <mas1529@nyu.edu>
2015-07-07 20:24:26 -0400

    tweaks

--- boiler-plate/sketch.js
+++ boiler-plate/sketch.js
@@ -1,22 +1,8 @@
-var mic;
-var amp;
 
 function setup(){
-	createCanvas(windowWidth, windowHeight);
-	background(0);
 
-	mic = new p5.AudioIn();
-	amp = new p5.Amplitude();
-
-	amp.setInput(mic);
-	mic.start();
 }
 
 function draw(){
-	background(0);
 
-	var level = amp.getLevel();
-
-	console.log(level);
 }
-
